3df8f26a20f210d86a9e07f9cd0f787eb33a9de3
Mmy2000 <yahianada005@gmail.com>
2024-05-12 10:59:24 +0300

    finishing navbar styling

--- fisrt-project/src/componet/Navbar/Navbar.jsx
+++ fisrt-project/src/componet/Navbar/Navbar.jsx
@@ -3,12 +3,16 @@ import { Link, NavLink } from 'react-router-dom'
 
 export default function Navbar() {
   return (
-    <div className='container p-2 fixed top-0 right-0 left-0 mx-auto'>
-      <ul className='flex flex-wrap justify-around items-center'>
-        <li><NavLink className='text-slate-900 text-lg font-normal' to="">Home</NavLink></li>
-        <li><NavLink className='text-slate-900 text-lg font-normal' to="about">About</NavLink></li>
-        <li><NavLink className='text-slate-900 text-lg font-normal' to="portfolio">Portfolio</NavLink></li>
-        <li><NavLink className='text-slate-900 text-lg font-normal' to="contact">contact</NavLink></li>
+    <div className='container py-8 bg-[#2c3e50]  fixed flex justify-around   items-center top-0 right-0 left-0 mx-auto'>
+        <div>
+            <Link className='text-white text-3xl font-black hover:text-white   ' to="">START FRAMEWORK</Link>
+        </div>
+      <ul className='flex flex-wrap  justify-around items-center'>
+        
+        
+        <li><NavLink className='text-white text-lg  px-4 py-2 font-black hover:text-white' to="about">About</NavLink></li>
+        <li><NavLink className='text-white text-lg  px-4 py-2 font-black hover:text-white' to="portfolio">Portfolio</NavLink></li>
+        <li><NavLink className='text-white text-lg  px-4 py-2 font-black hover:text-white' to="contact">contact</NavLink></li>
       </ul>
     </div>
   )

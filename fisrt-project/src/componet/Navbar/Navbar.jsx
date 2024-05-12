5c5b292a0c5ab13f0365a1bd6e9e28e7c16afb0f
Mmy2000 <yahianada005@gmail.com>
2024-05-12 13:45:26 +0300

    finishing contact styling

--- fisrt-project/src/componet/Navbar/Navbar.jsx
+++ fisrt-project/src/componet/Navbar/Navbar.jsx
@@ -5,14 +5,14 @@ export default function Navbar() {
   return (
     <div className=' py-8 bg-[#2c3e50] z-50  fixed flex justify-around   items-center top-0 right-0 left-0 '>
         <div>
-            <Link className='text-white text-3xl font-black hover:text-white   ' to="">START FRAMEWORK</Link>
+            <Link className='text-white text-3xl font-black hover:text-white text-decoration-none   ' to="">START FRAMEWORK</Link>
         </div>
       <ul className='flex flex-wrap  justify-around items-center'>
         
         
-        <li><NavLink className='text-white text-lg  px-4 py-2 font-black hover:text-white' to="about">About</NavLink></li>
-        <li><NavLink className='text-white text-lg  px-4 py-2 font-black hover:text-white' to="portfolio">Portfolio</NavLink></li>
-        <li><NavLink className='text-white text-lg  px-4 py-2 font-black hover:text-white' to="contact">contact</NavLink></li>
+        <li><NavLink className='text-white text-lg  px-4 py-2 font-black text-decoration-none hover:text-white' to="about">About</NavLink></li>
+        <li><NavLink className='text-white text-lg  px-4 py-2 font-black hover:text-white text-decoration-none' to="portfolio">Portfolio</NavLink></li>
+        <li><NavLink className='text-white text-lg  px-4 py-2 font-black hover:text-white text-decoration-none' to="contact">contact</NavLink></li>
       </ul>
     </div>
   )
